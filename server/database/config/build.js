1b5cda95e20f2a3704d0f40bae3ce24a3ff16c21
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-25 15:49:53 +0300

    remove unnessary code

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -2,12 +2,9 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 
 const connection = require('../connection');
-const {NODE_ENV} = process.env;
 const dbBuild = () => {
   let sql = readFileSync(join(__dirname, 'build.sql')).toString();
-  if (NODE_ENV === 'test') {
     sql+= readFileSync(join(__dirname, 'fakeData.sql')).toString();
-  }
   return connection.query(sql);
 };
 module.exports = dbBuild;
