a343ca167b8c4e2c5ffc8b63396b1d8a5ad690e0
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-25 22:18:00 +0300

    sign up query #11

---
+++ server/database/quieres/account/signUp.js
@@ -0,0 +1,2 @@
+const connection = require('../../connection')
+module.exports = (userName,email,phone,password)=>connection.query('INSERT INTO users (userName,email,phone,password) VALUES ($1,$2,$3,$4)',[userName,email,phone,password])
