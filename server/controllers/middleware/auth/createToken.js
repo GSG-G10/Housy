836cda185f568aece4e0ed4d41f23c7b002e6102
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-25 18:52:26 +0300

    get info user from req object #9

--- server/controllers/middleware/auth/createToken.js
+++ server/controllers/middleware/auth/createToken.js
@@ -1,7 +1,7 @@
 const {sign} = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     //user => object (name, email, phone)
-       const payload = req.body.user;
+       const payload = req.user;
        const accessToken = sign(payload,process.env.ACCESS_TOKEN_SECRET);
        res.cookie('token', accessToken);
 }
