02cca9b429d09180d9af1e9983aa3f850252371c
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-26 14:54:21 +0300

    authentication function #9

--- server/controllers/middleware/auth/createToken.js
+++ server/controllers/middleware/auth/createToken.js
@@ -1,8 +1,15 @@
 const {sign} = require('jsonwebtoken');
 module.exports = (req, res, next) => {
        const payload = req.user;
-       const accessToken = sign(payload,process.env.ACCESS_TOKEN_SECRET);
+       try{
+        const accessToken = sign(payload,process.env.ACCESS_TOKEN_SECRET);
        res.status(201).cookie('token', accessToken, {
                  httpOnly: true
+       }).json({
+                     message: 'User created successfully'
        });
+       }catch(err){
+        return next(err)
+       }
+       
 }
