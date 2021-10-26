0afc69e4e103b9931e4ffebfca85710a2a5971ff
ameera abedalnaser <ameeraabed92@gmail.com>
2021-10-26 14:31:15 +0300

    update-fack-data related #6

--- server/database/config/adminsFack Data.sql
+++ server/database/config/adminsFack Data.sql
@@ -1,3 +1,4 @@
-insert into admins (username, password ,  email) values ('Bobbie Fendt', 'F1A1CC71ED10594F97B1B6CF94A00727', 'bfendt0@blogspot.com');
-insert into admins (username, password ,  email) values ('Noni Djurdjevic', 'F1A1CC71ED10594F97B1B6CF94A00727', 'ndjurdjevic1@icq.com');
-insert into admins (username, password ,  email) values ('Kizzie Maliphant', 'F1A1CC71ED10594F97B1B6CF94A00727', 'kmaliphant2@php.net');
+insert into admins (username, password ,  email) values ('ameera', 'F1A1CC71ED10594F97B1B6CF94A00727', 'ameera2021abed@gmail.com');
+insert into admins (username, password ,  email) values ('haroon', 'F1A1CC71ED10594F97B1B6CF94A00727', 'hro19502001@gmail.com');
+insert into admins (username, password ,  email) values ('elham', 'F1A1CC71ED10594F97B1B6CF94A00727', 'elham2000fadel@gmail.com');
+insert into admins (username, password ,  email) values ('sallah', 'F1A1CC71ED10594F97B1B6CF94A00727', 'mohmsal96@gmail.com');
