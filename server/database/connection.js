5a4f107342a7d77e51a5e7f83b97541dce1a0bca
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-27 23:58:11 +0300

    fix ssl  #11

--- server/database/connection.js
+++ server/database/connection.js
@@ -19,7 +19,7 @@ switch (NODE_ENV) {
 }
 const options = {
   connectionString: dbUrl,
-  ssl: false,
+  ssl: { rejectUnauthorized: false },
 
 };
 module.exports = new Pool(options);
--- server/utils/validation/signUpSchema.js
+++ server/utils/validation/signUpSchema.js
@@ -4,6 +4,5 @@ module.exports = joi.object({
   username: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().min(5).required(),
-  // like +911234567890
   phone: joi.string().length(10).required(),
 });
