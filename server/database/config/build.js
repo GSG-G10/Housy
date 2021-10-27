a359212ceb8b8782cbaefe39cf11ece8ac8c3a36
mohammedsalah7 <mhamad_m@windowslive.com>
2021-10-27 12:40:59 +0300

    prepar setting data base test and testing for endpoint /users #17

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -5,7 +5,10 @@ const connection = require('../connection');
 
 const dbBuild = () => {
   let sql = readFileSync(join(__dirname, 'build.sql')).toString();
-  sql += readFileSync(join(__dirname, 'fakeData.sql')).toString();
+  sql += readFileSync(join(__dirname, 'adminsFackData.sql')).toString();
+  sql += readFileSync(join(__dirname, 'agentsFackData.sql')).toString();
+  sql += readFileSync(join(__dirname, 'imagesFackData.sql')).toString();
+  sql += readFileSync(join(__dirname, 'imagesFackData.sql')).toString();
   return connection.query(sql);
 };
-module.exports = dbBuild;
+module.exports = { dbBuild };
---
+++ server/test/index.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const supertest = require('supertest');
+const app = require('../app');
+const { dbBuild } = require('../database/config/build');
+const connection = require('../database/connection');
+
+beforeEach(() => dbBuild());
+
+describe('All users', () => {
+  test('get all users', (done) => {
+    supertest(app)
+      .get('/api/v1/users')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.msg).toBe('You are get all users');
+        return done();
+      });
+  });
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+afterAll(() => connection.end());
