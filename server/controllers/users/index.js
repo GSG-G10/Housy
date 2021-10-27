8e86b40e9b996dd2b914bc262278531a31f6ca84
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-27 22:46:22 +0300

    resolve conflicts #11

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 const signup = require('./signup');
-
-module.exports = {
-  signup,
-};
-=======
 const userEstateshandler = require('./userEstates');
 const getAllUsers = require('./getAllUsers');
 
-module.exports = { getAllUsers, userEstateshandler };
->>>>>>> 1c56f7cf651c1916acd88ff05d55b958056b73aa
+module.exports = {
+  signup,
+  getAllUsers,
+  userEstateshandler,
+};
