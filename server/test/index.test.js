311435048b881021e16a792cdfde46d8a7e9ca8a
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-30 12:43:46 +0300

    change content responed msg for the best

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -7,60 +7,60 @@ const connection = require('../database/config/connection');
 beforeEach(() => dbBuild());
 afterAll(() => connection.end());
 
-// describe('Tests Server', () => {
-//   test('get all users', async () => {
-//     const res = await supertest(app)
-//       .get('/api/v1/users')
-//       .expect(200)
-//       .expect('Content-Type', /json/);
-//     return expect(6).toEqual(res.body.data.length);
-//   });
-// });
+describe('Tests Server', () => {
+  test('get all users', async () => {
+    const res = await supertest(app)
+      .get('/api/v1/users')
+      .expect(200)
+      .expect('Content-Type', /json/);
+    return expect(6).toEqual(res.body.data.length);
+  });
+});
 
-// describe('user estates', () => {
-//   test('get users estates', async () => {
-//     const res = await supertest(app)
-//       .get('/api/v1/users/3/estates')
-//       .expect(200)
-//       .expect('Content-Type', /json/);
+describe('user estates', () => {
+  test('get users estates', async () => {
+    const res = await supertest(app)
+      .get('/api/v1/users/3/estates')
+      .expect(200)
+      .expect('Content-Type', /json/);
 
-//     return expect(res.body).toEqual({
-//       data: [
-//         {
-//           id: 3,
-//           agent_id: 3,
-//           title: 'rhoncus sed vestibulum',
-//           price: '84726.00',
-//           description: 'massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in',
-//           type: 'Buy',
-//           category: 'House',
-//           street: '0824 Mcguire Way',
-//           city: 'Kungshamn',
-//           region: 'Sweden',
-//           bathrooms: 1,
-//           bedrooms: 3,
-//           rooms: 3,
-//           space: '235',
-//           approved: true,
-//           rate: 5,
-//           available: false,
-//         },
-//       ],
-//     });
-//   });
-// });
+    return expect(res.body).toEqual({
+      data: [
+        {
+          id: 3,
+          agent_id: 3,
+          title: 'rhoncus sed vestibulum',
+          price: '84726.00',
+          description: 'massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in',
+          type: 'Buy',
+          category: 'House',
+          street: '0824 Mcguire Way',
+          city: 'Kungshamn',
+          region: 'Sweden',
+          bathrooms: 1,
+          bedrooms: 3,
+          rooms: 3,
+          space: '235',
+          approved: true,
+          rate: 5,
+          available: false,
+        },
+      ],
+    });
+  });
+});
 
-// describe('user estates', () => {
-//   test('get users estates', async () => {
-//     const res = await supertest(app)
-//       .get('/api/v1/users/three/estates')
-//       .expect(404)
-//       .expect('Content-Type', /json/);
-//     return expect(res.body).toEqual({
-//       message: 'enter valid user id',
-//     });
-//   });
-// });
+describe('user estates', () => {
+  test('get users estates', async () => {
+    const res = await supertest(app)
+      .get('/api/v1/users/three/estates')
+      .expect(404)
+      .expect('Content-Type', /json/);
+    return expect(res.body).toEqual({
+      message: 'enter valid user id',
+    });
+  });
+});
 
 describe('test signup endpoint with all cases ', () => {
   test('test sign up endpoint when success', async () => {
