cff70d8842916513a79c6024f63f59c575a28b22
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-30 12:58:27 +0300

    change code for the best

--- server/controllers/estates/deleteEstate.js
+++ server/controllers/estates/deleteEstate.js
@@ -3,14 +3,14 @@ const { deleteEstate } = require('../../database/quieres');
 module.exports = async (req, res, next) => {
   const { estateId } = req.params;
   // check is number
-  if (!Number.isInteger(estateId) || estateId <= 0) {
+  if (!(estateId > 0)) {
     return res.status(400).json({
       message: 'Invalid estate id',
     });
   }
   try {
-    const row = await deleteEstate(estateId);
-    if (row.rowCount > 0) {
+    const { rowCount } = await deleteEstate(estateId);
+    if (rowCount > 0) {
       res.status(200).json({
         message: 'Estate deleted successfully',
       });
