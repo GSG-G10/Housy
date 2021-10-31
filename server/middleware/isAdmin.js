c54d834cde8f072c6b073d9aa0b9fce0d3869fce
mohammedsalah7 <mhamad_m@windowslive.com>
2021-10-31 10:30:53 +0200

    edit Suggested changes #37

--- server/middleware/isAdmin.js
+++ server/middleware/isAdmin.js
@@ -8,12 +8,12 @@ const isAdmin = async (req, res, next) => {
       return res.status(400).json({ message: 'You are not authorized' });
     }
     const decoded = await verifyToken(token);
+    req.email = decoded.email;
 
-    const rows = await checkAdminQuery(decoded.email);
+    const rows = await checkAdminQuery(req.email);
     if (!rows.length) {
       return res.status(400).json({ message: 'You are not authorized' });
     }
-    res.status(201).json({ message: 'You are Admin' });
     return next();
   } catch (err) {
     return next(err);
