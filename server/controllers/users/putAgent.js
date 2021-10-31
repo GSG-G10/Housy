6b1142ae097293fdceda7a712f3a7f634d25d008
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-31 14:10:16 +0300

    edit user #10

--- server/controllers/users/putAgent.js
+++ server/controllers/users/putAgent.js
@@ -4,38 +4,7 @@ const agentSchema = require('../../utils/validation/agentSchema');
 module.exports = async (req, res, next) => {
   try {
     const { agentId } = req.params;
-    const data = await agentSchema.validate(
-      { ...req.body, studentId },
-      { abortEarly: false },
-    );
-    const result = await putStudent(data);
-    if (result.rowCount === 1) {
-      res.json({
-        StatusCode: 200,
-        data: {
-          message: "Student's data updated successfully",
-        },
-      });
-    } else {
-      res.status(404).json({
-        StatusCode: 404,
-        data: {
-          message: 'There is no student for this id',
-        },
-      });
-    }
   } catch (err) {
-    if (err.errors) {
-      res.status(400).json({ statusCode: 400, data: { message: err.errors } });
-    } else if (err.detail) {
-      res.status(409).json({
-        statusCode: 409,
-        data: {
-          message: err.detail,
-        },
-      });
-    } else {
-      next(err);
-    }
+    next(err);
   }
 };
