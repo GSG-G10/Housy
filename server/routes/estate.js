ab48d036a1f8a8e385a7a95d57103de7130a605e
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-31 19:02:30 +0300

    remove route delete

--- server/routes/estate.js
+++ server/routes/estate.js
@@ -4,5 +4,4 @@ const { editEstate, deleteEstate } = require('../controllers');
 
 router.put('/:estateId', editEstate);
 router.delete('/:estateId', isAuth, isAdmin, deleteEstate);
-router.delete('/:estateId', deleteEstate);
 module.exports = router;
