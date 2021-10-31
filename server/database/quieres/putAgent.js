d0a79a5152d4b98c7f10e0d073821832acebd17f
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-31 14:27:33 +0300

    query for update #10

--- server/database/quieres/putAgent.js
+++ server/database/quieres/putAgent.js
@@ -1,1 +1,5 @@
 const connection = require('../config/connection');
+
+module.exports = ({
+  agentId, name, email, phone, avater,
+}) => connection.query('UPDATE agent SET name=$1,email=$2,phone=$3,avater=$3 WHERE id=$4', [name, email, phone, avater, agentId]);
