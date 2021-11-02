b71db396ea346b6a98fc0a6e22b2137190301b22
ElhamFadel <elham2000Fadel@gmail.com>
2021-11-02 22:33:01 +0300

    card componetn design

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,3 +1,9 @@
 export { default as marker } from './marker.svg';
 export { default as money } from './money.svg';
 export { default as room } from './Solid.svg';
+export { default as bath } from './bath.png';
+export { default as beds } from './beds.png';
+export { default as couch } from './couch.png';
+export { default as image } from './image.jpg';
+export { default as vector } from './Vector.svg';
+export { default as foregroundIMG } from './foreground.png';
--- client/src/components/CardHouse/index.js
+++ client/src/components/CardHouse/index.js
@@ -1,39 +1,73 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import IconButton from '@mui/material/IconButton';
+import {
+  image, foregroundIMG, bath, couch, beds,
+} from '../../assets';
 import './style.css';
 
 export default function MediaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <IconButton aria-label="add to favorites">
-          <FavoriteBorderIcon color="primary" />
-        </IconButton>
-        <Typography variant="body2" color="text.secondary">
-          Beautiful Huge 1 Family House In Heart Of Westbury.
-          Newly Renovated With New Wood Floors,Beautiful Huge 1 Family House In Heart Of Westbury.
-          Newly Renovated With New Wood Floors,
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card className="card-house" sx={{ maxWidth: 290 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="green iguana"
+        />
+        <div className="card-type">For Sale</div>
+        <CardContent>
+          <div className="row">
+            <Typography gutterBottom variant="h4" component="div">
+              $100.00
+            </Typography>
+            <IconButton aria-label="add to favorites">
+              <FavoriteBorderIcon color="primary" />
+            </IconButton>
+          </div>
+          <Typography gutterBottom variant="h3" component="div">
+            Shami House
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Beautiful Huge 1 Family House In Heart Of Westbury. Newly Renovated With New
+            Wood Floors,
+            Newly Renovated With New Wood Floors,
+          </Typography>
+        </CardContent>
+        <div className="card-details">
+          <div className="row">
+            <div className="col">
+              <img src={couch} alt="number room" />
+              <span>
+                2 RM
+              </span>
+            </div>
+            <div className="col">
+              <img src={beds} alt="number beds" />
+              <span>
+                2 BR
+              </span>
+            </div>
+            <div className="col">
+              <img src={bath} alt="number bath" />
+              <span>
+                2 Bth
+              </span>
+            </div>
+            <div className="col">
+              <img src={foregroundIMG} alt="area" />
+              <span>
+                1200 Sqft
+              </span>
+            </div>
+          </div>
+        </div>
+
+      </Card>
+    </>
   );
 }
