d868c906bbcf7f342d84bd2a7f2843804ab5f07f
Elham Fadel <65178119+ElhamFadel@users.noreply.github.com>
2021-11-02 17:12:02 +0300

    Update users.js
    add login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  userEstateshandler, getAllUsers, putAgent,
+  userEstateshandler, getAllUsers, putAgent,login
 } = require('../controllers');
 const estate = require('./estate');
 const signup = require('../controllers/users/signup');
@@ -8,6 +8,7 @@ const { isAuth } = require('../middleware');
 
 router.use('/estate', estate);
 router.post('/signup', signup);
+router.post('/login', login);
 router.get('/:userId/estates', userEstateshandler);
 router.put('/:userId', isAuth, putAgent);
 router.get('/', getAllUsers);
