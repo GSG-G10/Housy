c11750723245e45d357f57885000912b34329678
Elham Fadel <65178119+ElhamFadel@users.noreply.github.com>
2021-11-02 17:10:07 +0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const estate = require('./estate');
 const users = require('./users');
 const admins = require('./admin');
-const { logout, login } = require('../controllers');
+const { logout } = require('../controllers');
 
 router.get('/logout', logout);
-router.post('/login', login);
 router.use('/admin', admins);
 router.use('/estate', estate);
 router.use('/user', users);
