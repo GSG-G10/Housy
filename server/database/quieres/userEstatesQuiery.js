77d3074a04aff2b1ca389d00d45bd4c6c70a1678
mohammedsalah7 <mhamad_m@windowslive.com>
2021-11-03 12:36:50 +0200

    edit qurey to select all data like agent info. , estate agent #80

--- server/database/quieres/userEstatesQuiery.js
+++ server/database/quieres/userEstatesQuiery.js
@@ -1,3 +1,3 @@
 const connection = require('../config/connection');
 
-module.exports = (userId) => connection.query('SELECT * FROM estates WHERE agent_id = $1', [userId]);
+module.exports = (userId) => connection.query('SELECT * FROM agents INNER JOIN estates  on agents.id = estates.agent_id INNER JOIN images ON estates.id = images.estate_id WHERE agents.id = ($1) ', [userId]);
