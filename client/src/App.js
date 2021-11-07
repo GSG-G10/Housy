f506223305a15dc124a31805f9e0840d68778a9b
mohammedsalah7 <mohmsal96@gmail.com>
2021-11-07 12:32:44 +0200

    create router in app , and format style #40

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/Navbar';
+
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+
+      <Router>
+        <NavBar />
+
+      </Router>
+    </div>
 
   );
 }
