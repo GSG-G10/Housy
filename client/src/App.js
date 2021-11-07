5ba39c8266a0e2dda3daddbd6e5667f85edd8387
muhammadharoun <hro19502001@gmail.com>
2021-11-07 14:50:35 +0200

    fix rectangle

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,6 @@ function App() {
 
       <Router>
         <NavBar />
-        <PropertyCard data={{
-          type: 'ss', price: '50', beds: '3', baths: '3', rooms: '10', space: '1520',
-        }}
-        />
 
       </Router>
       <Footer />
