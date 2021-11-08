3353ec5f07ac5926be08444c573f6952793b30d6
ElhamFadel <elham2000Fadel@gmail.com>
2021-11-08 13:10:30 +0300

    test filter estate controller #82

--- server/controllers/estates/filterEstate.js
+++ server/controllers/estates/filterEstate.js
@@ -2,8 +2,17 @@ const { filterEstates } = require('../../database/quieres');
 
 module.exports = async (req, res, next) => {
   try {
-    const estates = await filterEstates(req.params);
-    console.log(estates);
+    const {
+      query: {
+        type, category, location, price, roomNumbers, bathRooms, space,
+      },
+    } = req;
+    const { rows } = await filterEstates({
+      type, category, location, price, roomNumbers, bathRooms, space,
+    });
+    res.json({
+      data: rows,
+    });
   } catch (err) {
     next(err);
   }
--- server/database/quieres/filterEstates.js
+++ server/database/quieres/filterEstates.js
@@ -1,21 +1,38 @@
 const connection = require('../config/connection');
 
 module.exports = ({
-  type = 'sale',
-  category = 'whole house',
-  location = 'Gaza',
-  minPrice = 190450,
-  maxPrice = 190483.22,
-  numberOfRooms = 2,
-  numberOfBathrooms = 2,
-  numberOfBedrooms = 3,
-}) => connection.query(`
-    SELECT * FROM estates
-    WHERE type = $1
-    OR category = $2
-    OR city = $3
-    OR price BETWEEN $4 AND $5
-    OR rooms = $6
-    OR bathrooms = $7
-    OR bedrooms = $8
-    `, [type, category, location, minPrice, maxPrice, numberOfRooms, numberOfBathrooms, numberOfBedrooms]);
+  type, category, location, price, roomNumbers, bathRooms, space,
+}) => {
+  let text = 'SELECT * FROM estates WHERE ';
+  const values = [];
+
+  if (type) {
+    text += `type = $${values.length + 1} AND `;
+    values.push(type);
+  }
+  if (category) {
+    text += `category = $${values.length + 1} AND `;
+    values.push(category);
+  }
+  if (location) {
+    text += `location = $${values.length + 1} AND `;
+    values.push(location);
+  }
+  if (price) {
+    text += `price >= $${values.length + 1} AND `;
+    values.push(price);
+  }
+  if (roomNumbers) {
+    text += `rooms >= $${values.length + 1} AND `;
+    values.push(roomNumbers);
+  }
+  if (bathRooms) {
+    text += `bathrooms >= $${values.length + 1} AND `;
+    values.push(bathRooms);
+  }
+  if (space) {
+    text += `space >= $${values.length + 1} AND `;
+    values.push(space);
+  }
+  return connection.query({ text: text.slice(0, -4), values });
+};
