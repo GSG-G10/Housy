a4e86ab1202bf01b8333dc0d74f3932c1eec7e41
Elham Fadel <65178119+ElhamFadel@users.noreply.github.com>
2021-11-09 08:38:15 +0300

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,3 @@
 export { default as Search } from './Search';
-<<<<<<< HEAD
 export { default as CardHouse } from './CardHouse';
-=======
 export { default as Footer } from './Footer';
->>>>>>> bd9b5bfc5e4604324830c85f38b4d2402a280d6a
